202f6bf68f35648fbee29faadd0449ad69f9bdf5
Evert Pot <me@evertpot.com>
2022-11-05 20:40:53 -0400

    Working!

--- jneko.js
+++ jneko.js
@@ -34,7 +34,16 @@ const imageNames = [
 ];
 
 const nekoSize = 64;
+let nekoImg;
 
+let state;
+
+let favicon = document.querySelector("link[rel~='icon']");
+if (!favicon) {
+  favicon = document.createElement('link');
+  favicon.rel = 'icon';
+  document.getElementsByTagName('head')[0].appendChild(favicon);
+}
 const images = Object.fromEntries(imageNames.map(name => {
   const image = new Image(nekoSize, nekoSize);
   image.src = 'bitmaps/' + name + '.png';
@@ -45,7 +54,113 @@ const images = Object.fromEntries(imageNames.map(name => {
 function main() {
 
   const nekoDiv = document.getElementById('jneko');
-  nekoDiv.appendChild(images.sleep1);
+
+  nekoDiv.style.position = 'fixed';
+  nekoDiv.style.left = '500px';
+  nekoDiv.style.top = '300px';
+  nekoImg = new Image(nekoSize, nekoSize);
+  nekoDiv.appendChild(nekoImg);
+  nekoDiv.addEventListener('click', () => onClick());
+
+  setState('sleep');
+}
+
+const stateMachine = {
+  sleep: {
+    image: ['sleep1', 'sleep2'],
+    imageInterval: 1,
+    click: 'awake'
+  },
+  awake: {
+    image: 'awake',
+    nextState: 'normal',
+    nextStateDelay: 2.5,
+  },
+  normal: {
+    image: 'mati2',
+    nextState: ['normal', 'normal', 'normal', 'tilt', 'scratch', 'yawn'],
+    nextStateDelay: 1,
+  },
+  tilt: {
+    image: 'jare2',
+    nextState: 'normal',
+    nextStateDelay: 1,
+  },
+  yawn: {
+    image: 'mati3',
+    nextState: ['normal', 'sleep'],
+    nextStateDelay: 1,
+  },
+  scratch: {
+    image: ['kaki1', 'kaki2'],
+    imageInterval: 0.1,
+    nextState: 'normal',
+    nextStateDelay: 3,
+  }
+
+
+};
+
+let animationIndex = 0;
+
+function renderImage() {
+
+
+  let name = stateMachine[state].image;
+
+  animationIndex++;
+  if (Array.isArray(name)) {
+    name = name[animationIndex % name.length];
+  }
+  console.log('Rendering %s', name);
+  nekoImg.src = images[name].src;
+  favicon.href = nekoImg.src;
+}
+
+function sleep(s) {
+  return new Promise(res => setTimeout(res, s*1000));
+}
+
+let nextStateTimeout;
+let imageCycleInterval;
+
+function setState(stateName) {
+
+  clearTimeout(nextStateTimeout);
+  clearInterval(imageCycleInterval);
+
+  if (Array.isArray(stateName)) {
+    stateName = stateName[Math.floor(Math.random()*stateName.length)];
+  }
+  if (!stateMachine[stateName]) {
+    throw new Error('Uknown state: ' + stateName);
+  }
+  state = stateName;
+  const stateData = stateMachine[state];
+
+  if (stateData.nextState) {
+    nextStateTimeout = setTimeout(
+      () => setState(stateData.nextState),
+      stateData.nextStateDelay * 1000
+    );
+  }
+  renderImage();
+  if (stateData.imageInterval) {
+
+    imageCycleInterval = setInterval(
+      () => renderImage(),
+      stateData.imageInterval*1000
+    );
+
+  }
+
+}
+
+function onClick() {
+  const stateData = stateMachine[state];
+  if (stateData.click) {
+    setState(stateData.click);
+  }
 
 }
 
---
+++ readme.md
@@ -0,0 +1,12 @@
+jNeko - Javascript implementation of Neko
+=========================================
+
+Neko is a desktop pet originall written by Naoshi Watanabe for the NEC PC-8901.
+Kenji Gotoh created a Macintosh version in 1989, which has formed the basis of
+many ports.
+
+Over 30 years later, here's a simple Javascript version. This version doesn't
+follow the mouse pointer around, and stays asleep unless awakened.
+
+All the art is taken from Oneko, which is public domain. Thank you for Tatsuya
+Kato for creating oneko and Masayuki Koba for creating xneko.
